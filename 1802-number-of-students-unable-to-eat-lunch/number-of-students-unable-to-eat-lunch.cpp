135b816b684a7fab992a87ece0b34cd42e6b29d6
Iqrakhan5 <136209676+Iqrakhan5@users.noreply.github.com>
2024-08-29 20:29:02 +0500

    Time: 0 ms (100.00%) | Memory: 11.3 MB (7.14%) - LeetSync

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        stack<int>s;
+        queue<int>q;
+       reverse(sandwiches.begin(),sandwiches.end());
+       int count=0;
+       for(int& sd:sandwiches){
+        s.push(sd);
+       }
+       for(int& st:students){
+        q.push(st);
+       }
+       while(!s.empty() || !q.empty()){
+        if(s.top()==q.front()){
+            s.pop() , q.pop();
+            count=0;      }
+            else{
+                q.push(q.front());
+                q.pop();
+                count++;
+            }
+if(count>=q.size()){
+    break;
+}
+       }
+       return q.size();
+    }
+};
